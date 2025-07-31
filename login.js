22cedbcc34332b02d2eab8bffe79938f4084a003
Hardik Raiyani <hardikraiyani410@gmail.com>
2025-08-01 00:07:10 +0530

    Update login.js

--- login.js
+++ login.js
@@ -6,12 +6,12 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
         const errorMsg = document.getElementById("error-msg");
 
         // Dummy credentials
-        const validEmail = "hraiyani";
-        const validPassword = "8866570426";
+        const validEmail = "hrsilver";
+        const validPassword = "bill";
 
         if (email === validEmail && password === validPassword) {
             // Redirect to the next page
-            window.location.href = "cv2.html";
+            window.location.href = "hrbill.html";
         } else {
             errorMsg.textContent = "Invalid email or password.";
         }
