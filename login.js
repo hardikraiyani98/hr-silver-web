cc0819a8c2dd5a4e54282ed1fa70349fea1e3662
Hardik Raiyani <hardikraiyani410@gmail.com>
2025-08-08 13:22:21 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,16 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
+ const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+document.getElementById("signInForm").addEventListener("submit", function (e) {
         e.preventDefault();
 
         const email = document.getElementById("email").value;
@@ -6,14 +18,14 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
         const errorMsg = document.getElementById("error-msg");
 
         // Dummy credentials
-        const validEmail = "hrsilver";
-        const validPassword = "bill";
+        const validEmail = "hrsilverrajkot@gmail.com";
+        const validPassword = "8866570426";
 
         if (email === validEmail && password === validPassword) {
             // Redirect to the next page
             window.location.href = "hrbill.html";
         } else {
-            errorMsg.textContent = "Invalid email or password.";
-                alert("Invalid email or password");
+            errorMsg.textContent = "Invalid email or password";
+            alert("Invalid email or password");
         }
         });
